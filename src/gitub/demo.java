bdc1092ad0ac68ff172617c093f0dbeb1600b0a3
parsu <parsu@DESKTOP-D5V58MU>
2022-12-04 10:15:40 +0530

    [parsure]demo class created

---
+++ src/gitub/demo.java
@@ -0,0 +1,28 @@
+package gitub;
+
+public class demo 
+{
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("hi");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
