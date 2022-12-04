b0655fedecf712f70a6cfdd58c9bf12a50beebb8
parsu <parsu@DESKTOP-D5V58MU>
2022-12-04 10:39:41 +0530

    [parsure]demo2 is created

---
+++ src/gitub/demo2.java
@@ -0,0 +1,25 @@
+package gitub;
+
+public class demo2 
+{
+	public static void main(String[] args)
+	{
+		
+		
+		System.out.println("hi hello good morning");
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
